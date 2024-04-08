ffb91206a2d85aa0ff20228ab9e73e67cb373f7a
mnunderscore <33391370+mnunderscore@users.noreply.github.com>
2024-04-08 14:56:26 +0200

    Update index.html and app.js files

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,25 @@
-const myTextSlider = document.querySelector(".myTextSlider");
-const input = document.querySelector("#text-slider");
+// const myTextSlider = document.querySelector(".myTextSlider");
+// const input = document.querySelector("#text-slider");
 
 function updateText(e) {
-    myTextMouse = document.querySelector(".myTextMouse");
+    myTextMouse = document.querySelectorAll(".myTextMouse");
 	multiplierWidth = e.offsetX / window.innerWidth;
 	multiplierHeight = e.offsetY / window.innerHeight;
 	randomWeight = multiplierWidth * (600 - 35) + 35;
 	randomWidth = multiplierHeight * (600 - 100) + 100;
-	myTextMouse.style.fontVariationSettings = "\"wght\" " + randomWeight + ", \"wdth\" " + randomWidth;
+	myTextMouse.forEach((element) => element.style.fontVariationSettings = "\"wght\" " + randomWeight + ", \"wdth\" " + randomWidth)
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    const myTextMouse = document.querySelector(".myTextMouse"); // Add this line to select the element
-    if (!myTextMouse) return; // Check if the element exists before proceeding
+    const myTextMouse = document.querySelectorAll(".myTextMouse");
+    if (!myTextMouse) return;
 
 	window.addEventListener("mousemove", updateText);
 
-	const input = document.querySelector("#text-slider");
-	const myTextSlider = document.querySelector(".myTextSlider");
+	// const input = document.querySelector("#text-slider");
+	// const myTextSlider = document.querySelector(".myTextSlider");
 
-	input.addEventListener("input", (event) => {
-		myTextSlider.style.fontVariationSettings = "\"wght\" " + event.target.value;
-	});
+	// input.addEventListener("input", (event) => {
+	// 	myTextSlider.style.fontVariationSettings = "\"wght\" " + event.target.value;
+	// });
 });
