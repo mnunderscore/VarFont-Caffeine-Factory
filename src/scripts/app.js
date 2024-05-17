3a405a399d6a6258f7e18037f00b63dad6dc20b4
mnunderscore <33391370+mnunderscore@users.noreply.github.com>
2024-05-17 10:18:31 +0200

    feat: Update index.html with new text and headings

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,22 +4,33 @@ import Splitting from "splitting";
 
 window.addEventListener("DOMContentLoaded", () => {
   function updateText(e) {
-    myTextMouse = document.querySelectorAll(".move-on-mouse");
+    moveMouse = document.querySelectorAll(".move-on-mouse");
     let multiplierWidth = e.pageX / document.body.clientWidth;
-    let multiplierHeight = e.pageY / document.body.clientHeight;
     let randomWeight = multiplierWidth * (1000 - 35) + 35;
-    let randomWidth = multiplierHeight * (1000 - 100) + 100;
-    myTextMouse.forEach(
+    moveMouse.forEach(
       (element) =>
-        (element.style.fontVariationSettings =
-          '"wght" ' + randomWeight + ', "wdth" ' + randomWidth)
+        (element.style.fontVariationSettings = '"wght" ' + randomWeight)
     );
   }
 
-  let myTextMouse = document.querySelectorAll(".move-on-mouse");
-  if (!myTextMouse) return;
+  function updateTextSCAN(e) {
+    moveMouseScan = document.querySelectorAll(".move-on-mouse-scan");
+    let multiplierWidth = e.pageX / document.body.clientWidth;
+    let randomWeight = multiplierWidth * 50 - 30;
+    moveMouseScan.forEach(
+      (element) =>
+        (element.style.fontVariationSettings = '"SCAN" ' + randomWeight)
+    );
+  }
+
+  let moveMouse = document.querySelectorAll(".move-on-mouse");
+  if (!moveMouse) return;
+
+  let moveMouseScan = document.querySelectorAll(".move-on-mouse-scan");
+  if (!moveMouseScan) return;
 
   window.addEventListener("mousemove", updateText);
+  window.addEventListener("mousemove", updateTextSCAN);
 
   Splitting();
 
@@ -27,12 +38,14 @@ window.addEventListener("DOMContentLoaded", () => {
   let i = 0;
   setInterval(function () {
     crush.forEach((cru) => {
-      cru.style.fontVariationSettings = '"wght" 300';
+      cru.style.fontVariationSettings = '"SCAN" -50';
       cru.style.opacity = "0.5";
+      cru.style.color = "white";
     });
     crush[i].style.opacity = "1";
-    crush[i].style.fontVariationSettings = '"wght" 800';
+    crush[i].style.fontVariationSettings = '"SCAN" 10';
+    crush[i].style.color = "#ff0000";
     i++;
     if (i == crush.length) i = 0;
-  }, 400);
+  }, 800);
 });
